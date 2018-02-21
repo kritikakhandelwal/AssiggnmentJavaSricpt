79530ed47a4c360cc5876b761446da63cd63a5a8
Kritikakhandelwal <kritika.khandelwal@knoldus.in>
2018-02-21 17:44:44 +0530

    assignment

--- src/main/js/script2.js
+++ src/main/js/script2.js
@@ -1,9 +1,1 @@
- var username=document.forms["myForm"]["fname"];
-      username.addEventListner("blur",nameVerify,true);
-
-      function Validate(){
-      if(username.value == ""){
-               name_error.textContent="username is required";
-               return false;
-      }
-   }
+ 
